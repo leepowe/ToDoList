35aecd6ed8635e1f0127abb28f2e602ed6443f0f
Lee Powell <81479565+leepowe@users.noreply.github.com>
2023-12-22 23:08:26 -0500

    having a TypeError: will look into this more later before continuing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,52 @@
-import "./styles.css"
+import { useState } from 'react';
+import './styles.css';
+
+const [newItem, setNewItem] = useState('');
+const [todos, setTodos] = useState([]);
+
+function handleSubmit(e) {
+	e.preventDefault();
+
+	setTodos([
+		...todos,
+		{ id: crypto.randomUUID(), title: newItem, completed: false },
+	]);
+}
+
+console.log(todos);
 
 export default function App() {
-  return (
-    <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
-      <h1 className="header">To Do List</h1>
-      <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger"> Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger"> Delete</button>
-        </li>
-      </ul>
-    </>
-  )
+	return (
+		<>
+			<form onSubmit={handleSubmit} className='new-item-form'>
+				<div className='form-row'>
+					<label htmlFor='item'>New Item</label>
+					<input
+						value={newItem}
+						onChange={(e) => setNewItem(e.target.value)}
+						type='text'
+						id='item'
+					/>
+				</div>
+				<button className='btn'>Add</button>
+			</form>
+			<h1 className='header'>To Do List</h1>
+			<ul className='list'>
+				<li>
+					<label>
+						<input type='checkbox' />
+						Item 1
+					</label>
+					<button className='btn btn-danger'> Delete</button>
+				</li>
+				<li>
+					<label>
+						<input type='checkbox' />
+						Item 2
+					</label>
+					<button className='btn btn-danger'> Delete</button>
+				</li>
+			</ul>
+		</>
+	);
 }
